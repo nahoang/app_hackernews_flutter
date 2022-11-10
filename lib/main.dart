bdcac9e21a212f74b93082e7ea43d90cfe19c4f0
nahoang <nahoang9698@gmail.com>
2022-11-10 14:55:10 +0700

    Implement hn_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:hn_app/src/article.dart';
 import 'package:hn_app/src/hn_bloc.dart';
-import 'package:hn_app/src/loading_info.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -19,21 +18,15 @@ class MyApp extends StatelessWidget {
     this.bloc,
   }) : super(key: key);
 
-  static const primaryColor = Colors.white;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primaryColor: primaryColor,
-          scaffoldBackgroundColor: primaryColor,
-          canvasColor: Colors.black,
-          textTheme: Theme.of(context).textTheme.copyWith(
-              caption: TextStyle(color: Colors.white54),
-              subhead: TextStyle(fontFamily: 'Garamond', fontSize: 10.0))),
+        primarySwatch: Colors.blue,
+      ),
       home: MyHomePage(
-        title: 'Flutter Hacker News',
+        title: 'Flutter Demo Home Page',
         bloc: bloc,
       ),
     );
@@ -52,61 +45,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
-        leading: LoadingInfo(widget.bloc.isLoading),
-        elevation: 0.0,
-        actions: [
-          Builder(
-            builder: (context) => IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () async {
-                    final Article result = await showSearch(
-                      context: context,
-                      delegate: ArticleSearch(widget.bloc.articles),
-                    );
-                    if (result != null && await canLaunch(result.url)) {
-                      launch(result.url);
-                    }
-                  },
-                ),
-          ),
-        ],
       ),
       body: StreamBuilder<UnmodifiableListView<Article>>(
         stream: widget.bloc.articles,
         initialData: UnmodifiableListView<Article>([]),
         builder: (context, snapshot) => ListView(
-              children: snapshot.data.map(_buildItem).toList(),
-            ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            title: Text('Top Stories'),
-            icon: Icon(Icons.arrow_drop_up),
-          ),
-          BottomNavigationBarItem(
-            title: Text('New Stories'),
-            icon: Icon(Icons.new_releases),
-          ),
-        ],
-        onTap: (index) {
-          if (index == 0) {
-            widget.bloc.storiesType.add(StoriesType.topStories);
-          } else {
-            widget.bloc.storiesType.add(StoriesType.newStories);
-          }
-          setState(() {
-            _currentIndex = index;
-          });
-        },
+          children: snapshot.data.map(_buildItem).toList(),
+        ),
       ),
     );
   }
@@ -114,131 +64,27 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildItem(Article article) {
     return Padding(
       key: Key(article.title),
-      padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 12.0),
+      padding: const EdgeInsets.all(16.0),
       child: ExpansionTile(
         title:
-            Text(article.title ?? '[null]', style: TextStyle(fontSize: 24.0)),
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Text('${article.descendants} comments'),
-                SizedBox(width: 16.0),
-                IconButton(
-                  icon: Icon(Icons.launch),
-                  onPressed: () async {
-                    if (await canLaunch(article.url)) {
-                      launch(article.url);
-                    }
-                  },
-                )
-              ],
-            ),
+        Text(article.title ?? '[null]', style: TextStyle(fontSize: 24.0)),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(article.type),
+              IconButton(
+                icon: Icon(Icons.launch),
+                onPressed: () async {
+                  if (await canLaunch(article.url)) {
+                    launch(article.url);
+                  }
+                },
+              )
+            ],
           ),
         ],
       ),
-    );
-  }
-}
-
-class ArticleSearch extends SearchDelegate<Article> {
-  final Stream<UnmodifiableListView<Article>> articles;
-
-  ArticleSearch(this.articles);
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return StreamBuilder<UnmodifiableListView<Article>>(
-      stream: articles,
-      builder:
-          (context, AsyncSnapshot<UnmodifiableListView<Article>> snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-              child: Text(
-            'No data!',
-          ));
-        }
-
-        final results = snapshot.data
-            .where((a) => a.title.toLowerCase().contains(query.toLowerCase()));
-
-        return ListView(
-          children: results
-              .map<ListTile>((a) => ListTile(
-                    title: Text(a.title,
-                        style: Theme.of(context)
-                            .textTheme
-                            .subhead
-                            .copyWith(fontSize: 16.0)),
-                    leading: Icon(Icons.book),
-                    onTap: () async {
-                      if (await canLaunch(a.url)) {
-                        await launch(a.url);
-                      }
-                      close(context, a);
-                    },
-                  ))
-              .toList(),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return StreamBuilder<UnmodifiableListView<Article>>(
-      stream: articles,
-      builder:
-          (context, AsyncSnapshot<UnmodifiableListView<Article>> snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-              child: Text(
-            'No data!',
-          ));
-        }
-
-        final results = snapshot.data
-            .where((a) => a.title.toLowerCase().contains(query.toLowerCase()));
-
-        return ListView(
-          children: results
-              .map<ListTile>((a) => ListTile(
-                    title: Text(a.title,
-                        style: Theme.of(context).textTheme.subhead.copyWith(
-                              fontSize: 16.0,
-                              color: Colors.blue,
-                            )),
-                    onTap: () {
-                      close(context, a);
-                    },
-                  ))
-              .toList(),
-        );
-      },
     );
   }
 }
--- lib/src/hn_bloc.dart
+++ lib/src/hn_bloc.dart
@@ -2,89 +2,50 @@ import 'dart:async';
 import 'dart:collection';
 
 import 'package:hn_app/src/article.dart';
-import 'package:http/http.dart' as http;
 import 'package:rxdart/rxdart.dart';
-
-class HackerNewsApiError extends Error {
-  final String message;
-
-  HackerNewsApiError(this.message);
-}
+import 'package:http/http.dart' as http;
 
 class HackerNewsBloc {
-  HashMap<int, Article> _cachedArticles;
-  static const _baseUrl = 'https://hacker-news.firebaseio.com/v0/';
-
-  final _isLoadingSubject = BehaviorSubject<bool>(seedValue: false);
-
   final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
 
   var _articles = <Article>[];
 
-  final _storiesTypeController = StreamController<StoriesType>();
-
-  HackerNewsBloc() {
-    _cachedArticles = HashMap<int, Article>();
-    _initializeArticles();
-
-    _storiesTypeController.stream.listen((storiesType) async {
-      _getArticlesAndUpdate(await _getIds(storiesType));
-    });
-  }
+  List<int> _ids = [
+    17392995,
+    17397852,
+    17395342,
+    17385291,
+    17387851,
+    17395675,
+    17387438,
+    17393560,
+    17391971,
+    17392455,
+  ];
 
   Stream<UnmodifiableListView<Article>> get articles => _articlesSubject.stream;
 
-  Stream<bool> get isLoading => _isLoadingSubject.stream;
-
-  Sink<StoriesType> get storiesType => _storiesTypeController.sink;
-
-  Future<void> _initializeArticles() async {
-    _getArticlesAndUpdate(await _getIds(StoriesType.topStories));
+  HackerNewsBloc() {
+    _updateArticles().then((_) {
+      _articlesSubject.add(UnmodifiableListView(_articles));
+    });
   }
 
-  void close() {
-    _storiesTypeController.close();
-  }
 
   Future<Article> _getArticle(int id) async {
-    if (!_cachedArticles.containsKey(id)) {
-      final storyUrl = '${_baseUrl}item/$id.json';
-      final storyRes = await http.get(storyUrl);
-      if (storyRes.statusCode == 200) {
-        _cachedArticles[id] = parseArticle(storyRes.body);
-      } else {
-        throw HackerNewsApiError("Article $id couldn't be fetched.");
-      }
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
     }
-    return _cachedArticles[id];
   }
 
-  _getArticlesAndUpdate(List<int> ids) async {
-    _isLoadingSubject.add(true);
-    await _updateArticles(ids);
-
-    _articlesSubject.add(UnmodifiableListView(_articles));
-    _isLoadingSubject.add(false);
-  }
-
-  Future<List<int>> _getIds(StoriesType type) async {
-    final partUrl = type == StoriesType.topStories ? 'top' : 'new';
-    final url = '$_baseUrl${partUrl}stories.json';
-    final response = await http.get(url);
-    if (response.statusCode != 200) {
-      throw HackerNewsApiError("Stories $type couldn't be fetched.");
-    }
-    return parseTopStories(response.body).take(10).toList();
-  }
-
-  Future<Null> _updateArticles(List<int> articleIds) async {
-    final futureArticles = articleIds.map((id) => _getArticle(id));
+  Future<Null> _updateArticles() async {
+    final futureArticles = _ids.map((id) => _getArticle(id));
     final articles = await Future.wait(futureArticles);
     _articles = articles;
   }
-}
 
-enum StoriesType {
-  topStories,
-  newStories,
+
+
 }
